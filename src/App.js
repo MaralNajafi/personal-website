91c76d48435f432fa5a628b4b83aa29673091383
Maral Najafi <marawlnajafi@gmail.com>
2023-03-27 13:33:06 +0430

    initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style/Reset.css";
+import "./style/UtilityClasses.css";
+import "./style/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <section class="header-section">
+        <div class="inner-container">
+          <header class="main-header d-flex jc-space-between ai-center">
+            <h1>My website</h1>
+            <nav class="main-nav">
+              <ul class="d-flex ai-center">
+                <li class="active">
+                  <a href="#">home</a>
+                </li>
+                <li>
+                  <a href="#">about</a>
+                </li>
+                <li>
+                  <a href="#">contact</a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <div class="showcase d-grid">
+            <div class="showcase__info">
+              <div>
+                <span>This is me</span>
+                <h2>Maral Najafi</h2>
+                <p class="my-position">front end developer</p>
+                <p class="my-explanation">
+                  Creating, using computer tools, has always been my number 1
+                  passion. I was a senior business management student when I was
+                  first introduced to front-end web development. A journey to
+                  create, using computers' languages!
+                </p>
+                <button class="btn-primary">about me</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
