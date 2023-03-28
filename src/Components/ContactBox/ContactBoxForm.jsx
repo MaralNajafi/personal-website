88e3b34d2e951247efb08d663878c794fe3f742d
Maral Najafi <marawlnajafi@gmail.com>
2023-03-28 23:38:24 +0330

    add: form validation using yup

--- src/Components/ContactBox/ContactBoxForm.jsx
+++ src/Components/ContactBox/ContactBoxForm.jsx
@@ -1,5 +1,6 @@
 import { useFormik } from "formik";
 import React from "react";
+import * as Yup from "yup";
 
 export default function ContactBoxForm({ title }) {
   const formik = useFormik({
@@ -9,6 +10,15 @@ export default function ContactBoxForm({ title }) {
       subject: "",
       message: "",
     },
+    validationSchema: Yup.object({
+      fullName: Yup.string()
+        .max(30, "too long, must be less than 30ch*")
+        .min(10, "too short, must be more than 10ch*")
+        .required("required*"),
+      email: Yup.string().email("invalid email address").required("required*"),
+      subject: Yup.string().required("required*"),
+      message: Yup.string().required("required*"),
+    }),
     onSubmit: (values) => {
       console.log(values);
     },
@@ -20,40 +30,57 @@ export default function ContactBoxForm({ title }) {
         onSubmit={formik.handleSubmit}
         className="d-flex flex-col flex-grow-1"
       >
-        <input
-          name="fullName"
-          type="text"
-          className="stack-input"
-          placeholder="Full Name"
-          value={formik.values.fullName}
-          onChange={formik.handleChange}
-        />
-        <input
-          name="email"
-          type="email"
-          className="stack-input"
-          placeholder="email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-        />
+        <div className="d-flex flex-col">
+          <input
+            name="fullName"
+            type="text"
+            className="stack-input"
+            placeholder="Full Name"
+            value={formik.values.fullName}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.fullName ? (
+            <p className="error-message">{formik.touched.fullName && formik.errors.fullName}</p>
+          ) : null}
+        </div>
+        <div className="d-flex flex-col">
+          <input
+            name="email"
+            type="text"
+            className="stack-input"
+            placeholder="email"
+            value={formik.values.email}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          {formik.errors.email ? (
+            <p className="error-message">{formik.touched.email && formik.errors.email}</p>
+          ) : null}
+        </div>
 
         <div className="message-box d-flex flex-col flex-grow-1">
           <div className="d-flex message-box__subject">
             <input
               name="subject"
               type="text"
-              className="flex-grow-1"
-              placeholder="subject"
+              className={`"flex-grow-1" ${
+                formik.touched.subject && formik.errors.subject ? "error-style" : ""
+              }`}
+              placeholder={formik.touched.subject && formik.errors.subject ? "subject*" : "subject"}
               value={formik.values.subject}
+              onBlur={formik.handleBlur}
               onChange={formik.handleChange}
             />
           </div>
           <div className="d-flex flex-grow-1 message-box__message">
             <textarea
               name="message"
-              placeholder="Message"
+              placeholder={formik.touched.message && formik.errors.message ? "message*" : "message"}
               value={formik.values.message}
+              onBlur={formik.handleBlur}
               onChange={formik.handleChange}
+              className={formik.touched.message && formik.errors.subject ? "error-style" : ""}
             ></textarea>
           </div>
         </div>
