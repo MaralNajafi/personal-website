23e0bba7a74a3656caae2df80d05541602877251
Maral Najafi <marawlnajafi@gmail.com>
2023-03-29 19:27:16 +0330

    change: showcase component name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Background from "../Components/Background";
-import Showcase from "../Components/Boxes/Showcase";
+import ShowcaseBox from "../Components/Boxes/ShowcaseBox";
 export default function Home() {
   return (
     <Background waveMode={"home"}>
-      <Showcase />
+      <ShowcaseBox />
     </Background>
   );
 }
