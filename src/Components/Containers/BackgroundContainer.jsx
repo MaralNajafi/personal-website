20b3b444393c06e689e13d2875dc5bc42e312f13
Maral Najafi <marawlnajafi@gmail.com>
2023-04-01 20:38:31 +0330

    add: themeContext

--- src/Components/Containers/BackgroundContainer.jsx
+++ src/Components/Containers/BackgroundContainer.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import wave from "../../style/media/wave.svg";
 import wave2 from "../../style/media/wave-2.svg";
+import ThemeContext from "../../context/ThemeContext";
 
 export default function Background({ children, waveMode }) {
+  const themeData = useContext(ThemeContext);
   return (
     <div
       className="background flex-grow-1"
@@ -10,6 +12,7 @@ export default function Background({ children, waveMode }) {
         backgroundImage: `url(${
           waveMode === "home" ? wave : waveMode === "contact" ? wave2 : ""
         })`,
+        backgroundColor: themeData.theme === "light" ? "#fff" : "#000",
       }}
     >
       {children}
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -3,15 +3,18 @@ import Container from "../Components/Containers/Container";
 import InnerContainer from "../Components/Containers/InnerContainer";
 import MainHeader from "../Components/Header/MainHeader";
 import SVGContainer from "../Components/Containers/SVGContainer";
+import ThemeContext from "../context/ThemeContext";
 const Layout = () => {
   return (
-    <Container>
-      <SVGContainer />
-      <InnerContainer>
-        <MainHeader title={"My website"} />
-      </InnerContainer>
-      <Outlet />
-    </Container>
+    <ThemeContext.Provider value={{theme: "light"}}>
+      <Container>
+        <SVGContainer />
+        <InnerContainer>
+          <MainHeader title={"My website"} />
+        </InnerContainer>
+        <Outlet />
+      </Container>
+    </ThemeContext.Provider>
   );
 };
 
