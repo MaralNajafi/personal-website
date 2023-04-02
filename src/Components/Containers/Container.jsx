06c1235977fbb073d93e7a6344b9d08597fc30e7
Maral Najafi <marawlnajafi@gmail.com>
2023-04-02 20:25:57 +0330

    style: delete background

--- src/Components/Containers/Container.jsx
+++ src/Components/Containers/Container.jsx
@@ -1,19 +1,5 @@
 import React from "react";
-import wave from "../../style/media/wave.svg";
-import wave2 from "../../style/media/wave-2.svg";
 
-
-export default function Container({ children, path }) {
-  return (
-    <div
-      className="container d-flex flex-col background"
-      style={{
-        backgroundImage: `url(${
-          path === "/" ? wave : path === "/contact" ? wave2 : ""
-        })`,
-      }}
-    >
-      {children}
-    </div>
-  );
+export default function Container({ children }) {
+  return <div className="container d-flex flex-col background">{children}</div>;
 }
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,12 +4,10 @@ import InnerContainer from "../Components/Containers/InnerContainer";
 import MainHeader from "../Components/Header/MainHeader";
 import SVGContainer from "../Components/Containers/SVGContainer";
 import ThemeContext from "../context/ThemeContext";
-import { useLocation } from "react-router-dom";
 const Layout = () => {
-  const location = useLocation();
   return (
     <ThemeContext.Provider value={{ theme: "light" }}>
-      <Container path={location.pathname}>
+      <Container>
         <SVGContainer />
         <InnerContainer>
           <MainHeader title={"My website"} />
