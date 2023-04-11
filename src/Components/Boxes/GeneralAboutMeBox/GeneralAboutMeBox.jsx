a03764c62ea2cd3330e9017972078cbabab16ba4
Maral Najafi <marawlnajafi@gmail.com>
2023-04-11 13:37:05 +0330

    update: About page

--- src/Components/Boxes/GeneralAboutMeBox/GeneralAboutMeBox.jsx
+++ src/Components/Boxes/GeneralAboutMeBox/GeneralAboutMeBox.jsx
@@ -7,83 +7,81 @@ import MY_AGE from "../../../utils/getMyAge";
 
 export default function GeneralAboutMeBox() {
   return (
-    <>
-      <div className="general-about-me-box d-flex flex-col">
-        <div className="general-about-me-box__image"></div>
-        <div className="general-about-me-box__info d-flex flex-col flex-grow-1">
-          <div className="d-flex flex-col gap-5px white-space-nw">
-            <h3>General info</h3>
-            <KeyValueTypeBox keyName={"age"} valueContent={MY_AGE} />
-            <KeyValueTypeBox keyName={"born"} valueContent={"1999 July 1"} />
-            <KeyValueTypeBox
-              keyName={"location"}
-              valueContent={"Tehran/Iran"}
-            />
-            <KeyValueTypeBox
-              keyName={"position"}
-              valueContent={"front-end web developer"}
-            />
-          </div>
-          <div className="d-flex flex-col gap-5px">
-            <h3>Programming skills</h3>
-            <div className="d-flex flex-row flex-wrap gap-10px">
-              <Tag>HTML5</Tag>
-              <Tag>CSS3</Tag>
-              <Tag>Sass</Tag>
-              <Tag>JavaScript</Tag>
-              <Tag>React.js</Tag>
-              <Tag>Bootstrap</Tag>
-              <Tag>Git & Github</Tag>
-              <Tag>To be continued...</Tag>
-            </div>
-          </div>
+    <div className="general-about-me-box d-flex flex-col">
+      <div className="general-about-me-box__info d-flex flex-col flex-grow-1">
+        <div className="d-flex flex-col gap-5px white-space-nw">
+          <h3>General info</h3>
+          <KeyValueTypeBox keyName={"age"} valueContent={MY_AGE} />
+          <KeyValueTypeBox keyName={"born"} valueContent={"1999 July 1"} />
+          <KeyValueTypeBox keyName={"location"} valueContent={"Tehran/Iran"} />
+          <KeyValueTypeBox
+            keyName={"position"}
+            valueContent={"front-end web developer"}
+          />
+        </div>
 
-          <div>
-            <h3>Projects</h3>
-            <div className="d-flex flex-col gap-10px white-space-nw">
-              <Link
-                target="_blank"
-                to={"https://github.com/MaralNajafi/crypto-currency-website"}
-                className="td-underline"
-              >
-                crypto-currency-website
-              </Link>
-              <Link
-                target="_blank"
-                to={"https://github.com/MaralNajafi/news-website"}
-                className="td-underline"
-              >
-                news-website
-              </Link>
-              <Link
-                target="_blank"
-                to={"https://github.com/MaralNajafi/make-flash-cards"}
-                className="td-underline"
-              >
-                make-flash-cards
-              </Link>
-              <Link
-                target="_blank"
-                to={"https://github.com/MaralNajafi/rock-paper-scissors-game"}
-                className="td-underline"
-              >
-                rock-paper-scissors-game
-              </Link>
-              <Link
-                target={"_blank"}
-                to={"https://github.com/MaralNajafi/countdown-timer"}
-                className="td-underline"
-              >
-                countdown-timer
-              </Link>
-            </div>
+        <div>
+          <h3>Projects</h3>
+          <div className="d-flex flex-col gap-10px white-space-nw">
+            <Link
+              target="_blank"
+              to={"https://github.com/MaralNajafi/crypto-currency-website"}
+              className="td-underline"
+            >
+              crypto-currency-website
+            </Link>
+            <Link
+              target="_blank"
+              to={"https://github.com/MaralNajafi/news-website"}
+              className="td-underline"
+            >
+              news-website
+            </Link>
+            <Link
+              target="_blank"
+              to={"https://github.com/MaralNajafi/make-flash-cards"}
+              className="td-underline"
+            >
+              make-flash-cards
+            </Link>
+            <Link
+              target="_blank"
+              to={"https://github.com/MaralNajafi/rock-paper-scissors-game"}
+              className="td-underline"
+            >
+              rock-paper-scissors-game
+            </Link>
+            <Link
+              target={"_blank"}
+              to={"https://github.com/MaralNajafi/countdown-timer"}
+              className="td-underline"
+            >
+              countdown-timer
+            </Link>
           </div>
-          <div>
-            <h3>Languages</h3>
-            <div className="d-flex flex-col gap-10px white-space-nw"></div>
+        </div>
+        <div className="d-flex flex-col gap-5px">
+          <h3>Programming skills</h3>
+          <div className="d-flex flex-row flex-wrap gap-10px">
+            <Tag>JavaScript</Tag>
+            <Tag>React.js</Tag>
+            <Tag>Git & Github</Tag>
+            <Tag>HTML5</Tag>
+            <Tag>CSS3</Tag>
+            <Tag>SASS</Tag>
+            <Tag>To be continued...</Tag>
+          </div>
+        </div>
+
+        <div>
+          <h3>Languages</h3>
+          <div className="d-flex flex-row gap-10px white-space-nw">
+            <span>persian</span>
+            <span>English</span>
+            <span>French</span>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
