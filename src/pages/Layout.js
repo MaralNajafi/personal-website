7b4f3814f2c73a1bd56f4cc03705faa86acc7789
Ehsan Gazar <me@ehsangazar.com>
2023-04-18 08:51:11 +1000

    fixed some components

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,20 +1,20 @@
 import { Outlet } from "react-router-dom";
-import Container from "../Components/Container/Container";
-import MainHeader from "../Components/MainHeader/MainHeader";
-import MainFooter from "../Components/MainFooter/MainFooter";
-import SVGContainer from "../Components/SVGcontainer/SVGContainer";
+import Container from "../components/Container/Container";
+import MainHeader from "../components/MainHeader/MainHeader";
+import MainFooter from "../components/MainFooter/MainFooter";
+import SVGContainer from "../components/SVGcontainer/SVGContainer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Layout = () => {
   return (
-      <Container>
-        <ToastContainer position="top-right" />
-        <SVGContainer />
-        <MainHeader title={"My website"} />
-        <Outlet />
-        <MainFooter />
-      </Container>
+    <Container>
+      <ToastContainer position="top-right" />
+      <SVGContainer />
+      <MainHeader title={"My website"} />
+      <Outlet />
+      <MainFooter />
+    </Container>
   );
 };
 
